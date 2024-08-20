5de0e6656928bb4231098fbd2805a9361ea5d97d
Haseen Nurayin <78214651+haseennurayin@users.noreply.github.com>
2024-08-20 13:40:02 +0600

    Update main.py

--- Streamlit & FastAPI Combined News Application Development/FastAPI/main.py
+++ Streamlit & FastAPI Combined News Application Development/FastAPI/main.py
@@ -1,1 +1,25 @@
+from fastapi import FastAPI
+import uvicorn
+from app.routers import news, summary
 
+app = FastAPI(
+    title="AI-based News Summary API",
+    version="0.2",
+    description="API Documentation for Generating News Summaries Using AI.",
+    contact={
+        "name": "Haseen Nurayin",
+        "email": "haseennurayin@outlook.com",
+    },
+    redoc_url="/documentation",
+    docs_url="/endpoints",
+)
+
+app.include_router(news.router)
+app.include_router(summary.router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the News Summary API"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8011, reload=True)
