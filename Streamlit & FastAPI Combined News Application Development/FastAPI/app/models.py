5674828628e1d871385d0af63b844aee87330721
Haseen Nurayin <78214651+haseennurayin@users.noreply.github.com>
2024-08-20 13:53:38 +0600

    Update models.py

--- Streamlit & FastAPI Combined News Application Development/FastAPI/app/models.py
+++ Streamlit & FastAPI Combined News Application Development/FastAPI/app/models.py
@@ -5,31 +5,17 @@ from .database import Base
 class News(Base):
     __tablename__ = "news"
     id = Column(Integer, primary_key=True, index=True)
-    # publisher_website = Column(String, index=True)
     datetime = Column(DateTime)
     title = Column(String, index=True)
     body = Column(Text)
     link = Column(String)
-    
-    category_id = Column(Integer, ForeignKey('categories.id'))
-    reporter_id = Column(Integer, ForeignKey('reporters.id'))
-    publisher_id = Column(Integer, ForeignKey('publishers.id'))
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    reporter_id = Column(Integer, ForeignKey("reporters.id"))
+    publisher_id = Column(Integer, ForeignKey("publishers.id"))
 
     category = relationship("Category")
     reporter = relationship("Reporter")
     publisher = relationship("Publisher")
-
-    # @property
-    # def category_name(self):
-    #     return self.category.name if self.category else None
-
-    # @property
-    # def reporter_name(self):
-    #     return self.reporter.name if self.reporter else None
-
-    # @property
-    # def publisher_name(self):
-    #     return self.publisher.name if self.publisher else None
 
 class Category(Base):
     __tablename__ = "categories"
@@ -48,12 +34,12 @@ class Publisher(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True)
     email = Column(String, unique=True)
-    website = Column(String, nullable=True, unique=True)
+    website = Column(String, unique=True, nullable=True)
 
 class Image(Base):
     __tablename__ = "images"
     id = Column(Integer, primary_key=True, index=True)
-    news_id = Column(Integer, ForeignKey('news.id'))
+    news_id = Column(Integer, ForeignKey("news.id"))
     url = Column(String)
 
     news = relationship("News")
@@ -61,5 +47,5 @@ class Image(Base):
 class Summary(Base):
     __tablename__ = "summaries"
     id = Column(Integer, primary_key=True, index=True)
-    news_id = Column(Integer, ForeignKey('news.id'))
+    news_id = Column(Integer, ForeignKey("news.id"))
     summary_text = Column(Text)
