8a918c1102edb974eadf55812f15294c8dcb1fdb
Haseen Nurayin <78214651+haseennurayin@users.noreply.github.com>
2024-08-20 13:56:47 +0600

    Update utility.py

--- Streamlit & FastAPI Combined News Application Development/FastAPI/app/utility.py
+++ Streamlit & FastAPI Combined News Application Development/FastAPI/app/utility.py
@@ -3,16 +3,15 @@ from groq import Groq
 from dotenv import load_dotenv
 
 load_dotenv()
-# gro_api_key=os.getenv("GROQ_API_KEY")
 
-def generate_summary(news_body):
+def generate_summary(news_body: str) -> str:
     client = Groq()
     chat_completion = client.chat.completions.create(
         model="mixtral-8x7b-32768",
         messages=[
             {
                 "role": "system",
-                "content": "You are expert in news summarization in bengali languages. Please summarize the following news article in top  3-5 bullet points in the bengali language that you get."
+                "content": "You are an expert in news summarization in Bengali. Please summarize the following news article in 3-5 bullet points in Bengali."
             },
             {
                 "role": "user",
@@ -26,5 +25,3 @@ def generate_summary(news_body):
         stop=None,
     )
     return chat_completion.choices[0].message.content
-
-    
