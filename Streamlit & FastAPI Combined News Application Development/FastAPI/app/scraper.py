baa5cb181dfda44a1e9586646497bb856c911e53
Haseen Nurayin <78214651+haseennurayin@users.noreply.github.com>
2024-08-20 13:56:01 +0600

    Update scraper.py

--- Streamlit & FastAPI Combined News Application Development/FastAPI/app/scraper.py
+++ Streamlit & FastAPI Combined News Application Development/FastAPI/app/scraper.py
@@ -2,25 +2,22 @@ import datetime
 from requests_html import HTMLSession
 from .database import SessionLocal
 from .crud import create_news
-from .schemas import NewsCreate, News
+from .schemas import NewsCreate
 
-def single_news_scraper(url: str):
+def single_news_scraper(url: str) -> NewsCreate:
     session = HTMLSession()
     try:
         response = session.get(url)
-        #response.html.render()  # This will download Chromium if not found
 
         publisher_website = url.split('/')[2]
         publisher = publisher_website.split('.')[-2]
         title = response.html.find('h1', first=True).text
         reporter = response.html.find('.contributor-name', first=True).text
         datetime_element = response.html.find('time', first=True)
-        news_datetime = datetime_element.attrs['datetime']
+        news_datetime = datetime.datetime.fromisoformat(datetime_element.attrs['datetime'])
         category = response.html.find('.print-entity-section-wrapper', first=True).text
         content = '\n'.join([p.text for p in response.html.find('p')])
-        img_tags = response.html.find('img')
-        images = [img.attrs['src'] for img in img_tags if 'src' in img.attrs]
-        news_datetime = datetime.datetime.now()
+        images = [img.attrs['src'] for img in response.html.find('img') if 'src' in img.attrs]
 
         print(f"Scraped news from {url}")
         print(f"Title: {title}")
@@ -28,7 +25,6 @@ def single_news_scraper(url: str):
         print(f"Date: {news_datetime}")
         print(f"Category: {category}")
         print(f"Images: {images}")
-
 
         return NewsCreate(
             publisher_website=publisher_website,
@@ -46,14 +42,11 @@ def single_news_scraper(url: str):
     finally:
         session.close()
 
-def scrape_and_store_news(url: str, db: SessionLocal): # type: ignore
-    # db = SessionLocal()
+def scrape_and_store_news(url: str, db: SessionLocal):
     news_data = single_news_scraper(url)
     print(news_data)
     inserted_news = ""
     if news_data:
-        # print(news_data)
         inserted_news = create_news(db=db, news=news_data)
     db.close()
-
     return inserted_news
