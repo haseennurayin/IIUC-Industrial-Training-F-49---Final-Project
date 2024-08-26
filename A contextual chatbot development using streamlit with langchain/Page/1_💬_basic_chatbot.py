0e14ce6e96a30b1b03d42d6bcd875ab2047ca5b4
Haseen Nurayin <78214651+haseennurayin@users.noreply.github.com>
2024-08-26 12:06:01 +0600

    Update 1_💬_basic_chatbot.py

--- A contextual chatbot development using streamlit with langchain/Page/1_💬_basic_chatbot.py
+++ A contextual chatbot development using streamlit with langchain/Page/1_💬_basic_chatbot.py
@@ -1,7 +1,6 @@
 import utils
 import streamlit as st
 from streaming import StreamHandler
-
 from langchain.chains import ConversationChain
 
 st.set_page_config(page_title="Chatbot", page_icon="💬")
@@ -10,14 +9,12 @@ st.write('Allows users to interact with the LLM')
 st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot/blob/master/pages/1_%F0%9F%92%AC_basic_chatbot.py)')
 
 class BasicChatbot:
-
     def __init__(self):
         utils.sync_st_session()
         self.llm = utils.configure_llm()
     
     def setup_chain(self):
-        chain = ConversationChain(llm=self.llm, verbose=False)
-        return chain
+        return ConversationChain(llm=self.llm, verbose=False)
     
     @utils.enable_chat_history
     def main(self):
@@ -27,14 +24,10 @@ class BasicChatbot:
             utils.display_msg(user_query, 'user')
             with st.chat_message("assistant"):
                 st_cb = StreamHandler(st.empty())
-                result = chain.invoke(
-                    {"input":user_query},
-                    {"callbacks": [st_cb]}
-                )
+                result = chain.invoke({"input": user_query}, {"callbacks": [st_cb]})
                 response = result["response"]
                 st.session_state.messages.append({"role": "assistant", "content": response})
                 utils.print_qa(BasicChatbot, user_query, response)
 
 if __name__ == "__main__":
-    obj = BasicChatbot()
-    obj.main()
+    BasicChatbot().main()
