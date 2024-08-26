1604fc10ba5796b3effe9f299cca5e94d040d541
Haseen Nurayin <78214651+haseennurayin@users.noreply.github.com>
2024-08-26 12:06:50 +0600

    Update 2_⭐_context_aware_chatbot.py

--- A contextual chatbot development using streamlit with langchain/Page/2_⭐_context_aware_chatbot.py
+++ A contextual chatbot development using streamlit with langchain/Page/2_⭐_context_aware_chatbot.py
@@ -1,7 +1,6 @@
 import utils
 import streamlit as st
 from streaming import StreamHandler
-
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 
@@ -11,16 +10,14 @@ st.write('Enhancing Chatbot Interactions through Context Awareness')
 st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot/blob/master/pages/2_%E2%AD%90_context_aware_chatbot.py)')
 
 class ContextChatbot:
-
     def __init__(self):
         utils.sync_st_session()
         self.llm = utils.configure_llm()
     
     @st.cache_resource
-    def setup_chain(_self):
+    def setup_chain(self):
         memory = ConversationBufferMemory()
-        chain = ConversationChain(llm=_self.llm, memory=memory, verbose=False)
-        return chain
+        return ConversationChain(llm=self.llm, memory=memory, verbose=False)
     
     @utils.enable_chat_history
     def main(self):
@@ -30,14 +27,10 @@ class ContextChatbot:
             utils.display_msg(user_query, 'user')
             with st.chat_message("assistant"):
                 st_cb = StreamHandler(st.empty())
-                result = chain.invoke(
-                    {"input":user_query},
-                    {"callbacks": [st_cb]}
-                )
+                result = chain.invoke({"input": user_query}, {"callbacks": [st_cb]})
                 response = result["response"]
                 st.session_state.messages.append({"role": "assistant", "content": response})
                 utils.print_qa(ContextChatbot, user_query, response)
 
 if __name__ == "__main__":
-    obj = ContextChatbot()
-    obj.main()
+    ContextChatbot().main()
