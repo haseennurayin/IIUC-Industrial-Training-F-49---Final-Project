40a277a6213127d475a10ba37251d7c5cf97ca2a
Haseen Nurayin <78214651+haseennurayin@users.noreply.github.com>
2024-08-26 12:08:15 +0600

    Update 4_📄_chat_with_your_documents.py

--- A contextual chatbot development using streamlit with langchain/Page/4_📄_chat_with_your_documents.py
+++ A contextual chatbot development using streamlit with langchain/Page/4_📄_chat_with_your_documents.py
@@ -2,13 +2,11 @@ import os
 import utils
 import streamlit as st
 from streaming import StreamHandler
-
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import DocArrayInMemorySearch
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
 
 st.set_page_config(page_title="ChatPDF", page_icon="📄")
 st.header('Chat with your documents (Basic RAG)')
@@ -16,7 +14,6 @@ st.write('Has access to custom documents and can respond to user queries by refe
 st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot/blob/master/pages/4_%F0%9F%93%84_chat_with_your_documents.py)')
 
 class CustomDocChatbot:
-
     def __init__(self):
         utils.sync_st_session()
         self.llm = utils.configure_llm()
@@ -26,7 +23,6 @@ class CustomDocChatbot:
         folder = 'tmp'
         if not os.path.exists(folder):
             os.makedirs(folder)
-        
         file_path = f'./{folder}/{file.name}'
         with open(file_path, 'wb') as f:
             f.write(file.getvalue())
@@ -34,14 +30,12 @@ class CustomDocChatbot:
 
     @st.spinner('Analyzing documents..')
     def setup_qa_chain(self, uploaded_files):
-        # Load documents
         docs = []
         for file in uploaded_files:
             file_path = self.save_file(file)
             loader = PyPDFLoader(file_path)
             docs.extend(loader.load())
-        
-        # Split documents and store in vector db
+
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=1000,
             chunk_overlap=200
@@ -49,20 +43,17 @@ class CustomDocChatbot:
         splits = text_splitter.split_documents(docs)
         vectordb = DocArrayInMemorySearch.from_documents(splits, self.embedding_model)
 
-        # Define retriever
         retriever = vectordb.as_retriever(
             search_type='mmr',
-            search_kwargs={'k':2, 'fetch_k':4}
+            search_kwargs={'k': 2, 'fetch_k': 4}
         )
 
-        # Setup memory for contextual conversation        
         memory = ConversationBufferMemory(
             memory_key='chat_history',
             output_key='answer',
             return_messages=True
         )
 
-        # Setup LLM and QA chain
         qa_chain = ConversationalRetrievalChain.from_llm(
             llm=self.llm,
             retriever=retriever,
@@ -74,15 +65,12 @@ class CustomDocChatbot:
 
     @utils.enable_chat_history
     def main(self):
-
-        # User Inputs
         uploaded_files = st.sidebar.file_uploader(label='Upload PDF files', type=['pdf'], accept_multiple_files=True)
         if not uploaded_files:
             st.error("Please upload PDF documents to continue!")
             st.stop()
 
         user_query = st.chat_input(placeholder="Ask me anything!")
-
         if uploaded_files and user_query:
             qa_chain = self.setup_qa_chain(uploaded_files)
 
@@ -91,15 +79,14 @@ class CustomDocChatbot:
             with st.chat_message("assistant"):
                 st_cb = StreamHandler(st.empty())
                 result = qa_chain.invoke(
-                    {"question":user_query},
+                    {"question": user_query},
                     {"callbacks": [st_cb]}
                 )
                 response = result["answer"]
                 st.session_state.messages.append({"role": "assistant", "content": response})
                 utils.print_qa(CustomDocChatbot, user_query, response)
 
-                # to show references
-                for idx, doc in enumerate(result['source_documents'],1):
+                for idx, doc in enumerate(result['source_documents'], 1):
                     filename = os.path.basename(doc.metadata['source'])
                     page_num = doc.metadata['page']
                     ref_title = f":blue[Reference {idx}: *{filename} - page.{page_num}*]"
@@ -107,5 +94,4 @@ class CustomDocChatbot:
                         st.caption(doc.page_content)
 
 if __name__ == "__main__":
-    obj = CustomDocChatbot()
-    obj.main()
+    CustomDocChatbot().main()
